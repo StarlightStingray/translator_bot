a0b63d3c12525ae187c29e92565be7a073656fd9
StarlightStingray <somajstingray@gmail.com>
2022-09-07 13:48:17 -0700

    reads only non-bot messages

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use std::env;
 
+use libretranslate::{Language, Translate};
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
-iuse libretranslate::{translate, Language};
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+        if msg.content.len() > 0 && !msg.author.bot {
+            // let source = Language::French;
+            // let target = Language::English;
+            // let input = msg.content;
+
+            // let data = translate(Language::Detect, target, input, None).await.unwrap();
+
+            let data = msg
+                .content
+                .to_lang(Language::English)
+                .from_lang(Language::Japanese)
+                .url("https://libretranslate.de/")
+                .translate()
+                .await
+                .unwrap();
+
+            // let source = format!("Input {}: {}", data.source.as_pretty(), data.input);
+            // let translation = format!("Output {}: {}", data.target.as_pretty(), data.output);
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, data).await {
                 println!("Error sending message: {:?}?", why);
             }
         }
@@ -38,13 +56,4 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
-
-    // let source = Language::French;
-    let target = Language::English;
-    let input = "Le texte français.";
-
-    let data = translate(source, target, input, None).await.unwrap();
-
-    println!("Input {}: {}", data.source.as_pretty(), data.input);
-    println!("Output {}: {}", data.target.as_pretty(), data.output);
 }
